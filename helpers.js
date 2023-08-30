9e03244379ff428bce29f889603220cb86ea47fa
anicrob <anrobbins18@gmail.com>
2023-08-30 15:44:44 -0400

    removed unused import

--- helpers.js
+++ helpers.js
@@ -1,7 +1,6 @@
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 const fs = require("fs");
-const path = require("path");
 
 const getInactiveUsers = async () => {
   try {
