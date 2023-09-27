304ab72053d1630bb55d6a769097a0dfc86f7046
anicrob <arobbins@adaptavist.com>
2023-09-27 14:55:51 -0400

    jira script to delete projects

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-const {
-  getInactiveUsers,
-  findInactiveUserDashboards,
-  findInactiveUserFilters,
-  returnCSV,
-} = require("./helpers.js");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 require("dotenv").config();
 
-const script = async () => {
-  const inactiveUserIds = await getInactiveUsers();
-  if(inactiveUserIds){
-    const inactiveDashboards = await findInactiveUserDashboards(inactiveUserIds);
-    returnCSV(inactiveDashboards, "dashboards");
-    const inactiveFilters = await findInactiveUserFilters(inactiveUserIds);
-    returnCSV(inactiveFilters, "filters");
-  }
+const projectKeys = ["TDSP"];
+const deleteProjects = async () => {
+  await Promise.all(
+    projectKeys.map(async (key) => {
+      try {
+        const response = await fetch(
+          `${process.env.URL}/rest/api/3/project/${key}`,
+          {
+            method: "DELETE",
+            headers: {
+              Authorization: `Basic ${process.env.API_KEY}`,
+              Accept: "application/json",
+            },
+          }
+        );
+
+        if (!response.ok) {
+          const message = `An error has occured trying to delete project with key, ${key},: ${response.status} ${response.statusText}`;
+          throw new Error(message);
+        }
+        console.log(`project with key, ${key}, has been successfully deleted`);
+      } catch (err) {
+        console.log(err);
+      }
+    })
+  );
 };
 
-script();
+deleteProjects();
